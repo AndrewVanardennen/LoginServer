0a5f8446c826974aaab5066ebf3b614c4979bb0c
vandeurenglenn <vandeurenglenn@gmail.com>
2020-09-11 19:43:31 +0200

    Fix demo, pass params

--- src/client.js
+++ src/client.js
@@ -3,9 +3,9 @@ import client from './../node_modules/socket-request-client/src/index'
 export default async () => {
   const connection = await client('ws://localhost:4444', 'login')
   
-  const login = async () => await connection.request({
+  const login = async params => await connection.request({
     url: 'login',
-    params: { user: 'TESTUSER', password: 'TESTPASSWORD' }
+    params
   })
   
   return {
